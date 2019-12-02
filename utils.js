7f20b4137a0c6c58e6f4be281d4b2addea210d14
Alexey Guskov <alexey.guskov@nordnet.se>
2019-12-02 10:49:27 +0100

    02-1 done

--- utils.js
+++ utils.js
@@ -1,5 +1,9 @@
 const assert = (a, b) =>
-  console.log(a === b ? "test passed" : `test failed ${a} != ${b}`);
+  console.log(
+    JSON.stringify(a) === JSON.stringify(b)
+      ? "test passed"
+      : `test failed ${a} != ${b}`
+  );
 
 const file = name =>
   require("fs")
