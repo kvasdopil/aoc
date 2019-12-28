dc1c81effbf8bb0bd322efbebd06be969abdb825
Alexey Guskov <lexag@mail.ru>
2019-12-28 20:57:09 +0100

    14 wip

--- 2019/14/14-1.js
+++ 2019/14/14-1.js
@@ -1,11 +1,4 @@
-const p1 = [
-  "10 ORE => 10 A",
-  "1 ORE => 1 B",
-  "7 A, 1 B => 1 C",
-  "7 A, 1 C => 1 D",
-  "7 A, 1 D => 1 E",
-  "7 A, 1 E => 1 FUEL"
-];
+const { assert, file } = require("../../utils");
 
 const parse = lines => {
   const items = lines.map(line => {
@@ -26,37 +19,112 @@ const parse = lines => {
   );
 };
 
-const solve = transforms => {
+const solve = tf => {
+  const transforms = parse(tf);
   const resources = { FUEL: 1 };
 
-  const process = item => {
-    const reqs = transforms[item].req;
-    resources[item]--;
-    for (const req in reqs) {
-      if (resources[req] === undefined) {
-        resources[req] = reqs[req];
-      } else {
-        resources[req] += reqs[req];
-      }
-    }
-  };
-
-  while (true) {
-    let transformed = 0;
-    for (const item in resources) {
-      if (resources[item] > 0) {
-        if (item !== "ORE") {
-          process(item);
-          transformed++;
-          console.log(resources);
-          break;
+  const transform = () => {
+    for (const res in resources) {
+      if (res !== "ORE") {
+        if (resources[res] !== 0) {
+          if (transforms[res].amount <= resources[res]) {
+            resources[res] -= transforms[res].amount;
+            for (const res2 in transforms[res].req) {
+              if (resources[res2] === undefined) {
+                resources[res2] = 0;
+              }
+              resources[res2] += transforms[res].req[res2];
+            }
+            if (resources[res] === 0) {
+              delete resources[res];
+            }
+            return true;
+          }
         }
       }
     }
-    if (transformed === 0) {
-      return;
-    }
-  }
+    return false;
+  };
+
+  while (transform()) {}
+
+  return resources;
 };
 
-solve(parse(p1));
+const p1 = [
+  "10 ORE => 10 A",
+  "1 ORE => 1 B",
+  "7 A, 1 B => 1 C",
+  "7 A, 1 C => 1 D",
+  "7 A, 1 D => 1 E",
+  "7 A, 1 E => 1 FUEL"
+];
+
+console.log(solve(p1));
+
+const p2 = [
+  "9 ORE => 2 A",
+  "8 ORE => 3 B",
+  "7 ORE => 5 C",
+  "3 A, 4 B => 1 AB",
+  "5 B, 7 C => 1 BC",
+  "4 C, 1 A => 1 CA",
+  "2 AB, 3 BC, 4 CA => 1 FUEL"
+];
+
+console.log(solve(p2));
+
+const p3 = [
+  "157 ORE => 5 NZVS",
+  "165 ORE => 6 DCFZ",
+  "44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL",
+  "12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ",
+  "179 ORE => 7 PSHF",
+  "177 ORE => 5 HKGWZ",
+  "7 DCFZ, 7 PSHF => 2 XJWVT",
+  "165 ORE => 2 GPVTF",
+  "3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"
+];
+
+console.log(solve(p3));
+
+const p4 = [
+  "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG",
+  "17 NVRVD, 3 JNWZP => 8 VPVL",
+  "53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL",
+  "22 VJHF, 37 MNCFX => 5 FWMGM",
+  "139 ORE => 4 NVRVD",
+  "144 ORE => 7 JNWZP",
+  "5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC",
+  "5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV",
+  "145 ORE => 6 MNCFX",
+  "1 NVRVD => 8 CXFTF",
+  "1 VJHF, 6 MNCFX => 4 RFSQX",
+  "176 ORE => 6 VJHF"
+];
+
+console.log(solve(p4));
+
+const p5 = [
+  "171 ORE => 8 CNZTR",
+  "7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL",
+  "114 ORE => 4 BHXH",
+  "14 VRPVC => 6 BMBT",
+  "6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL",
+  "6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT",
+  "15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW",
+  "13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW",
+  "5 BMBT => 4 WPTQ",
+  "189 ORE => 9 KTJDG",
+  "1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP",
+  "12 VRPVC, 27 CNZTR => 2 XDBXC",
+  "15 KTJDG, 12 BHXH => 5 XCVML",
+  "3 BHXH, 2 VRPVC => 7 MZWV",
+  "121 ORE => 7 VRPVC",
+  "7 XCVML => 6 RJRHP",
+  "5 BHXH, 4 VRPVC => 5 LTCX"
+];
+
+console.log(solve(p5));
+
+console.log(solve(file("./14.txt")));
