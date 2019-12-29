4f66a3ca9e795d1521c3dfaba07120f189d84919
Alexey Guskov <lexag@mail.ru>
2019-12-29 15:36:44 +0100

    15 wip

---
+++ 2019/15/15-1.js
@@ -0,0 +1,78 @@
+const { assert, file } = require("../../utils");
+const { context, execute } = require("./intcode.js");
+
+const ct = context(file("./15.txt")[0]);
+const prog = execute(ct);
+prog.next();
+
+const map = { "0,0": 1 };
+const set = (x, y, val) => (map[`${pos.x + x},${pos.y + y}`] = val);
+const get = (x, y) => map[`${pos.x + x},${pos.y + y}`];
+
+const pos = { x: 0, y: 0 };
+
+const moves = {
+  1: { x: 0, y: -1 },
+  2: { x: 0, y: 1 },
+  3: { x: -1, y: 0 },
+  4: { x: 1, y: 0 }
+};
+
+const print = () => {
+  const chars = {
+    undefined: " ",
+    "-1": "X",
+    0: "#",
+    1: "."
+  };
+
+  for (let y = -10; y < 10; y++) {
+    const line = [];
+    for (let x = -20; x < 20; x++) {
+      const val = get(x, y);
+      if (x === 0 && y === 0) {
+        line.push("@");
+      } else {
+        line.push(chars[val]);
+      }
+    }
+    console.log(line.join(""));
+  }
+};
+
+const step = dir => {
+  const move = moves[dir];
+
+  prog.next(dir);
+  const value = ct.output.pop();
+
+  set(move.x, move.y, value);
+  if (value > 0) {
+    pos.x += move.x;
+    pos.y += move.y;
+  }
+
+  return value;
+};
+
+const decide = () => {
+  for (let i = 1; i <= 4; i++) {
+    move = moves[i];
+    if (get(move.x, move.y) === undefined) {
+      return step(i) === 2;
+    }
+  }
+
+  set(0, 0, -1);
+  for (let i = 1; i <= 4; i++) {
+    move = moves[i];
+    if (get(move.x, move.y) === 1) {
+      step(i);
+      return false;
+    }
+  }
+};
+
+while (!decide()) {}
+
+print();
