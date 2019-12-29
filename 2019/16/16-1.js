c2eb5dd18032ceed23cfdaa42f4acb5192fd4338
Alexey Guskov <lexag@mail.ru>
2019-12-29 19:19:08 +0100

    16 wip

---
+++ 2019/16/16-1.js
@@ -0,0 +1,54 @@
+const { assert, file } = require("../../utils");
+
+function* pattern(n) {
+  const pat = [0, 1, 0, -1];
+  let a = 0;
+  let b = 1;
+
+  while (true) {
+    while (b < n) {
+      yield pat[a % 4];
+      b++;
+    }
+    b = 0;
+    a++;
+  }
+}
+
+const process = input =>
+  input.map((val, n) => {
+    const p = pattern(n + 1);
+    return (
+      Math.abs(
+        input.map(val => val * p.next().value).reduce((a, b) => a + b, 0)
+      ) % 10
+    );
+  });
+
+const solve = (input, count) => {
+  let val = input.split("").map(n => parseInt(n, 10));
+  for (let i = 0; i < count; i++) {
+    val = process(val);
+  }
+  return val.join("");
+};
+
+assert(solve("12345678", 1), "48226158");
+assert(solve("12345678", 2), "34040438");
+assert(solve("12345678", 3), "03415518");
+assert(solve("12345678", 4), "01029498");
+
+assert(
+  solve("80871224585914546619083218645595", 100),
+  "24176176480919046114038763195595"
+); // 24176176
+assert(
+  solve("19617804207202209144916044189917", 100),
+  "73745418557257259149466599639917"
+); // 73745418
+assert(
+  solve("69317163492948606335995924319873", 100),
+  "52432133292998606880495974869873"
+); // 52432133
+
+console.log(solve(file("./16.txt")[0], 100));
