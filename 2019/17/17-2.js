62cdb3fb11763963fc31b0d85cf9830548e2cfd8
Alexey Guskov <lexag@mail.ru>
2019-12-30 23:14:40 +0100

    17 done

--- 2019/17/17-2.js
+++ 2019/17/17-2.js
@@ -1,54 +1,37 @@
 const { assert, file } = require("../../utils");
 const { context, execute } = require("./intcode.js");
 
-// const ct = context(file("./17.txt")[0]);
+const ct = context(file("./17.txt")[0]);
 
-// ct.mem[0] = 2;
+ct.mem[0] = 2;
 
-// const prog = execute(ct);
+const prog = execute(ct);
 
-// prog.next();
+prog.next();
 
-// const line = chars => {
-//   for (let i = 0; i < chars.length; i++) {
-//     prog.next(chars.charCodeAt(i));
-//   }
-//   prog.next(10);
-// };
+const line = chars => {
+  for (let i = 0; i < chars.length; i++) {
+    prog.next(chars.charCodeAt(i));
+  }
+  prog.next(10);
+};
 
-// //   |      prog here     |
-// line("A,C,C,C,C,C,C,B,C,C");
-// line("L");
-// line("R");
-// line("1");
+//   |      prog here     |
+line("A,B,B,C,B,C,B,C,A,A");
+line("L,6,R,8,L,4,R,8,L,12");
+line("L,12,R,10,L,4");
+line("L,12,L,6,L,4,L,4");
 
-// ct.output = [];
-// line("y");
+ct.output = [];
+line("y");
 
-// const map = ct.output
-//   .map(a => String.fromCharCode(a))
-//   .join("")
-//   .trim()
-//   .split("\n")
-//   .map(line => line.split(""));
+const map = ct.output
+  .map(a => String.fromCharCode(a))
+  .join("")
+  .trim()
+  .split("\n")
+  .map(line => line.split(""));
 
-// map.forEach(line => console.log(line.join("")));
+map.forEach(line => console.log(line.join("")));
 
-const cmds = ["A", "B", "C"];
-for (let c = 0; c < 3 * 3 * 3 * 3 * 3 * 3 * 3 * 3 * 3 * 3; c++) {
-  const vals = [
-    Math.floor(c / 1) % 3,
-    Math.floor(c / 3) % 3,
-    Math.floor(c / 3 / 3) % 3,
-    Math.floor(c / 3 / 3 / 3) % 3,
-    Math.floor(c / 3 / 3 / 3 / 3) % 3,
-    Math.floor(c / 3 / 3 / 3 / 3 / 3) % 3,
-    Math.floor(c / 3 / 3 / 3 / 3 / 3 / 3) % 3,
-    Math.floor(c / 3 / 3 / 3 / 3 / 3 / 3 / 3) % 3,
-    Math.floor(c / 3 / 3 / 3 / 3 / 3 / 3 / 3 / 3) % 3,
-    Math.floor(c / 3 / 3 / 3 / 3 / 3 / 3 / 3 / 3 / 3) % 3
-  ]
-    .map(v => cmds[v])
-    .join(",");
-  console.log(vals);
-}
+console.log(ct.output[ct.output.length - 1]);
