6a8597a9c2e35b6beee9e54d3e467e5a4e0187a5
Alexey Guskov <lexag@mail.ru>
2019-12-31 19:46:10 +0100

    16-2 wip

--- 2019/16/16-2.js
+++ 2019/16/16-2.js
@@ -15,77 +15,50 @@ function* pattern(n) {
   }
 }
 
-// const process = input =>
-//   input.map((_, n) => {
-//     const p = pattern(n + 1);
-//     const v = input.reduce((res, val) => res + val * p.next().value, 0);
-//     return Math.abs(v) % 10;
-//   });
+const add = (a, b) => a.map((vala, n) => vala + b[n]);
+const sub = (a, b) => a.map((vala, n) => vala - b[n]);
 
-const calculate = (numbers, n, generation) => {
-  if (generation === 0) {
-    return numbers[n];
-  }
+const process = input =>
+  input.map((val, n) => {
+    const p = pattern(n + 1);
 
-  let result = 0;
-  const p = pattern(n + 1);
-  for (let i = 0; i < numbers.length; i++) {
-    const m = p.next().value;
-    if (m > 0) {
-      result += calculate(numbers, i, generation - 1);
+    let res = input[0].map(() => 0);
+    for (let i = 0; i < input.length; i++) {
+      const multiplier = p.next().value;
+      if (multiplier > 0) {
+        res = add(res, input[i]);
+      }
+      if (multiplier < 0) {
+        res = sub(res, input[i]);
+      }
     }
-    if (m < 0) {
-      result -= calculate(numbers, i, generation - 1);
-    }
-  }
+    return res;
+  });
 
-  // let result = 0;
-  // for (let i = 0; i < n; i++) {
-  //   let j = n - 1 + i;
-  //   while (j < numbers.length) {
-  //     result += calculate(numbers, j, generation - 1);
-  //     j += n * 4;
-  //   }
-  //   j = n + n + n + i - 1;
-  //   while (j < numbers.length) {
-  //     result -= calculate(numbers, j, generation - 1);
-  //     j += n * 4;
-  //   }
-  // }
+let pat = [
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-  return Math.abs(result) % 10;
-};
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
 
-const solve = (input, gen, start = 0, len = 8) => {
-  const numbers = input.split("").map(n => parseInt(n, 10));
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
 
-  const res = [];
-  for (let i = 0; i < len; i++) {
-    res.push(calculate(numbers, start + i, gen));
-  }
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+];
 
-  return res.join("");
-};
+for (let i = 0; i < 120; i++) {
+  pat = process(pat);
+}
 
-assert(solve("12345678", 1), "48226158");
-assert(solve("12345678", 2), "34040438");
-assert(solve("12345678", 3), "03415518");
-assert(solve("12345678", 4), "01029498");
-
-assert(
-  solve("80871224585914546619083218645595", 100),
-  "24176176480919046114038763195595"
-); // 24176176
-// assert(
-//   solve("19617804207202209144916044189917", 100),
-//   "73745418557257259149466599639917"
-// ); // 73745418
-// assert(
-//   solve("69317163492948606335995924319873", 100),
-//   "52432133292998606880495974869873"
-// ); // 52432133
-
-// console.log(solve(file("./16.txt")[0].repeat(1), 100).substring(0, 8));
-// console.log(solve(file("./16.txt")[0].repeat(2), 100).substring(0, 8));
-// console.log(solve(file("./16.txt")[0].repeat(3), 100).substring(0, 8));
-// console.log(solve(file("./16.txt")[0].repeat(4), 100).substring(0, 8));
+pat.map(n => console.log(n.join()));
