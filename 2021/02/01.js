8da7cac9edfabb94bff3a4035a21449c7a61921d
Alexey Guskov <lexag@mail.ru>
2021-12-03 11:10:15 +0100

    2021/02 done

---
+++ 2021/02/01.js
@@ -0,0 +1,24 @@
+const { file, assert } = require('../../utils')
+
+const work = (lines) => {
+  let p = 0;
+  let d = 0;
+  lines.map(line => line.split(' ')).map(([c, arg]) => {
+    const val = parseInt(arg, 10);
+    if (c === 'forward') p += val;
+    if (c === 'up') d -= val;
+    if (c === 'down') d += val;
+  })
+  return p * d;
+}
+
+const test = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`.trim().split("\n");
+
+assert(work(test), 150);
+assert(work(file('input.txt')), 1815044);
+console.log(work(file('input.txt')))
---
+++ 2021/02/02.js
@@ -0,0 +1,28 @@
+const { file, assert } = require('../../utils')
+
+const work = (lines) => {
+  let p = 0;
+  let d = 0;
+  let aim = 0;
+  lines.map(line => line.split(' ')).map(([c, arg]) => {
+    const val = parseInt(arg, 10);
+    if (c === 'forward') {
+      p += val;
+      d += aim * val;
+    }
+    if (c === 'up') aim -= val;
+    if (c === 'down') aim += val;
+  })
+  return p * d;
+}
+
+const test = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`.trim().split("\n");
+
+assert(work(test), 900);
+assert(work(file('input.txt')), 1739283308);
+console.log(work(file('input.txt')))
