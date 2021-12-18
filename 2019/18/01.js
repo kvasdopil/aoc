f4247e75a0394a92431796b244b0a74fbdfa9381
Lexa <lexa@Ulias-MacBook-Pro.local>
2021-12-18 15:09:01 +0100

    2019/18 wip

--- 2019/18/01.js
+++ 2019/18/01.js
@@ -1,64 +1,6 @@
-const { assert } = require('../../utils');
-
-// 
-
-// const findPaths = (input, start) => {
-//   const result = input.map(line => line.map(ch => ch === '#' ? '#' : '.'));
-
-
-//   const [x, y] = start;
-//   result[y][x] = 0;
-
-//   let i = 0;
-
-//   while (true) {
-//     let working = false;
-//     result.forEach((line, y) =>
-//       line.forEach((ch, x) => {
-//         if (ch !== i) {
-//           return;
-//         }
-//         n.forEach(([xx, yy]) => {
-//           if (result[y + yy][x + xx] === '.') {
-//             working = true;
-//             result[y + yy][x + xx] = i + 1;
-//           }
-//         })
-//       })
-//     )
-//     if (!working) {
-//       break;
-//     }
-//     i++;
-//   }
-
-//   return result;
-// }
+const { assert, file } = require('../../utils');
 
 const directions = [[-1, 0], [1, 0], [0, 1], [0, -1]];
-// const wayfinder = (getFn, doneFn, start) => {
-//   let [x, y] = start;
-//   const map = {};
-//   map[`${x},${y}`] = { x, y, val: 0 };
-
-//   for (const pt in map) {
-//     if (map[pt].val === ct) {
-//       directions.forEach(([xx, yy]) => {
-//         const x2 = x + xx;
-//         const y2 = y + yy;
-//         const val = map[`${x2},${y2}`] || getFn(x2, y2);
-//         if (val === true) {
-//           if (doneFn(x2, y2)) {
-//             map[`${x2},${y2}`] = ct;
-//           } else {
-//             map[`${x2},${y2}`] = ct + 1;
-//           }
-//         }
-//       })
-//     }
-//   }
-// }
-
 const print = map => console.log(map.map(line => line.join('')).join("\n"))
 
 const findStart = (map) => {
@@ -93,40 +35,81 @@ const findWays = (map) => {
   return results;
 }
 
-const solve = (input, prefix, s) => {
+const work = (input) => {
   let map = input.map(line => line.split(""));
-  const path = [];
-
-  let ways = findWays(map)
-    .map(([x, y, letter, steps]) => {
-      const door = letter.toUpperCase();
-
-      const copy = map.map(line => line.map(ch => {
-        if (ch === door) return '.';
-        if (ch === letter) return '@';
-        if (ch === '@') return '.';
-        return ch;
-      }));
-      return solve(copy, prefix + letter, steps + s);
-    });
-
-
+  const [path, steps] = solve(map).sort(([, a], [, b]) => a - b).shift();
+  return steps;
 }
 
-const i1 = [
+const solve = (map, prefix = "", steps = 0) => {
+  const result = [];
+
+  let ways = findWays(map);
+  if (ways.length === 0) return [[prefix, steps]];
+
+  for (const [x, y, letter, s] of ways) {
+    const copy = applyKey(map, letter);
+    for (const next of solve(copy, prefix + letter, steps + s)) {
+      result.push(next);
+    }
+  }
+
+  return result;
+}
+
+const applyKey = (map, letter) => {
+  const door = letter.toUpperCase();
+  return map.map(line => line.map(ch => {
+    if (ch === door) return '.';
+    if (ch === letter) return '@';
+    if (ch === '@') return '.';
+    return ch;
+  }));
+}
+
+const t1 = [
   '#########',
   '#b.A.@.a#',
   '#########',
 ];
+assert(work(t1), 8);
 
-assert(solve(i1), "ab");
-
-const i2 = [
+const t2 = [
   '########################',
   '#f.D.E.e.C.b.A.@.a.B.c.#',
   '######################.#',
   '#d.....................#',
   '########################',
 ]
+assert(work(t2), 86);
 
-assert(solve(i2), "abcdef");
+const t3 = `
+  ########################
+  #...............b.C.D.f#
+  #.######################
+  #.....@.a.B.c.d.A.e.F.g#
+  ########################`.trim().split('\n');
+assert(work(t3), 132)
+
+const t4 = `
+########################
+#@..............ac.GI.b#
+###d#e#f################
+###A#B#C################
+###g#h#i################
+########################`.trim().split('\n');
+assert(work(t4), 81)
+
+const t5 = `
+#################
+#i.G..c...e..H.p#
+########.########
+#j.A..b...f..D.o#
+########@########
+#k.E..a...g..B.n#
+########.########
+#l.F..d...h..C.m#
+#################`.trim().split('\n');
+assert(work(t5), 136)
+
+assert(work(file('input.txt')), 1)
