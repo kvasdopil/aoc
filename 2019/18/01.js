ad599c5537ea028f29d39bf47ea8d8cca911b00c
Lexa <lexa@Ulias-MacBook-Pro.local>
2021-12-19 07:53:45 +0100

    2019/18 wip

--- 2019/18/01.js
+++ 2019/18/01.js
@@ -1,7 +1,11 @@
-const { assert, file } = require('../../utils');
+const { assert, file, permutation } = require('../../utils');
 
 const directions = [[-1, 0], [1, 0], [0, 1], [0, -1]];
-const print = map => console.log(map.map(line => line.join('')).join("\n"))
+const chm = (ch) => {
+  if (ch === '#') return ' ';
+  return ch;
+}
+const print = map => console.log(map.map(line => line.map(chm).join('')).join("\n"))
 
 const findStart = (map) => {
   const y = map.findIndex(line => line.includes("@"));
@@ -35,22 +39,73 @@ const findWays = (map) => {
   return results;
 }
 
-const work = (input) => {
-  let map = input.map(line => line.split(""));
-  const [path, steps] = solve(map).sort(([, a], [, b]) => a - b).shift();
-  return steps;
+const optimize = (map) => {
+  const o = () => {
+    for (let y = 1; y < map.length - 1; y++)
+      for (let x = 1; x < map[0].length - 1; x++) {
+        const walls = directions
+          .map(([dx, dy]) => map[y + dy][x + dx])
+          .filter(val => val === '#')
+          .length;
+        if (walls === 3 && (map[y][x] === '.' || (map[y][x] >= 'A' && map[y][x] <= 'Z'))) {
+          map[y][x] = '#';
+          return true;
+        }
+      }
+  }
+
+  while (o()) {
+    //    print(map);
+  }
+  print(map);
 }
 
-const solve = (map, prefix = "", steps = 0) => {
+const work = (input, limit = 999999) => {
+  let map = input.map(line => line.split(""));
+  optimize(map);
+  let min = 999999;
+  while (true) {
+    map = input.map(line => line.split(""));
+    let path = '';
+    let len = 0;
+
+    while (true) {
+      const res = solve({ map, limit }).sort(([, a], [, b]) => a - b);
+
+      const n = Math.floor(Math.random() * res.length / 50);
+      const [[prefix, steps]] = res[n];
+      path += prefix;
+      len += steps;
+
+      if (prefix.length < limit) break;
+
+      for (const letter of prefix.split(""))
+        map = applyKey(map, letter);
+    }
+
+    if (len < min) {
+      console.log(path, len);
+      min = len;
+    }
+  }
+  return len;
+}
+
+const solve = ({ map, prefix = "", steps = 0, limit }) => {
   const result = [];
 
+
   let ways = findWays(map);
-  if (ways.length === 0) return [[prefix, steps]];
+  if (ways.length === 0) return [[[prefix, steps]]];
 
   for (const [x, y, letter, s] of ways) {
-    const copy = applyKey(map, letter);
-    for (const next of solve(copy, prefix + letter, steps + s)) {
-      result.push(next);
+    if (limit === 0) {
+      result.push([[prefix + letter, steps + s]]);
+    } else {
+      const copy = applyKey(map, letter);
+      for (const next of solve({ map: copy, prefix: prefix + letter, steps: steps + s, limit: limit - 1 })) {
+        result.push(next);
+      }
     }
   }
 
@@ -72,7 +127,7 @@ const t1 = [
   '#b.A.@.a#',
   '#########',
 ];
-assert(work(t1), 8);
+// assert(work(t1), 8);
 
 const t2 = [
   '########################',
@@ -81,7 +136,7 @@ const t2 = [
   '#d.....................#',
   '########################',
 ]
-assert(work(t2), 86);
+// assert(work(t2), 86);
 
 const t3 = `
   ########################
@@ -89,7 +144,7 @@ const t3 = `
   #.######################
   #.....@.a.B.c.d.A.e.F.g#
   ########################`.trim().split('\n');
-assert(work(t3), 132)
+// assert(work(t3), 132)
 
 const t4 = `
 ########################
@@ -98,7 +153,7 @@ const t4 = `
 ###A#B#C################
 ###g#h#i################
 ########################`.trim().split('\n');
-assert(work(t4), 81)
+// assert(work(t4), 81)
 
 const t5 = `
 #################
@@ -110,6 +165,8 @@ const t5 = `
 ########.########
 #l.F..d...h..C.m#
 #################`.trim().split('\n');
-assert(work(t5), 136)
+// assert(work(t5, 2), 136)
 
-assert(work(file('input.txt')), 1)
+assert(work(file('input.txt'), 2), 1)
+
+// 6000 too high
