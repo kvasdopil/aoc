cafe97e9a1f0142f9ea842cede696b05f56ee284
Alexey Guskov <lexag@mail.ru>
2021-12-20 10:36:42 +0100

    2021/20 done

---
+++ 2021/20/01.js
@@ -0,0 +1,51 @@
+const { file, assert, array2d } = require('../../utils')
+
+const NS = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [-1, 0],
+  [0, 0],
+  [1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+]
+
+const step = (mapping, tiles) => {
+  const ngh = (x, y) => parseInt(NS.map(([dx, dy]) => {
+    let X = x + dx;
+    let Y = y + dy;
+    if (tiles[Y] === undefined) Y = y;
+    if (tiles[Y][X] === undefined) X = x;
+    return tiles[Y][X];
+  }).join(''), 2);
+
+  return tiles.map((line, y) => line.map((val, x) =>
+    mapping[ngh(x, y)] === '#' ? 1 : 0
+  ));
+}
+
+const work = (lines, count) => {
+  const mapping = lines.shift().trim();
+
+  const sz = 1000;
+  let tiles = array2d(sz, sz);
+
+  lines.forEach((line, y) => {
+    line.split('').forEach((val, x) => {
+      if (val === '#')
+        tiles[sz / 2 + y][sz / 2 + x] = 1;
+    })
+  });
+
+  while (count) {
+    tiles = step(mapping, tiles);
+    console.log(tiles.map(line => line.map(a => a ? '#' : ' ').join('')).join('\n'))
+    count--;
+  }
+  return tiles.map(line => line.filter(a => a).length).reduce((a, b) => a + b, 0);
+}
+
+assert(work(file('test.txt'), 2), 35);
+assert(work(file('input.txt'), 2), 5489);
